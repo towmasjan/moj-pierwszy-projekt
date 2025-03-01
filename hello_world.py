864c71c740265aedf76bfe99e1289b50af25c618
towmasjan <146456372+towmasjan@users.noreply.github.com>
2025-03-01 02:21:06 +0100

    Create hello_world.py
    dodaje nowy program

---
+++ hello_world.py
@@ -0,0 +1,7 @@
+def main():
+    print("Witaj świecie z mojego pierwszego projektu na GitHubie!")
+    imie = input("Jak masz na imię? ")
+    print(f"Miło Cię poznać, {imie}!")
+
+if __name__ == "__main__":
+    main()
